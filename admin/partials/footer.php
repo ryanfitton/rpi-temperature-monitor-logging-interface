46778f606b80d5bd0a28e2d7c2395e2e7a74868f
Ryan Fitton <ryan@ryanfitton.co.uk>
2020-09-03 20:12:05 +0100

    Loading jQuery from Composer package manager

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -3,11 +3,9 @@
     </div>
 
 
-    <script src="../assets/js/jquery-3.5.1.slim.min.js"></script>
-    <script>window.jQuery || document.write('<script src="..ssets/js/jquery-3.5.1.slim.min.js"><\/script>')</script>
+    <script src="../vendor/components/jquery/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../vendor/components/jquery/jquery.min.js"><\/script>')</script>
     
     <script src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.js"></script>
-    
-    <script src="../assets/js/feather.min.js"></script>
   </body>
 </html>
